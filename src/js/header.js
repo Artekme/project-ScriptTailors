4f1c4c742caf08f874f43b9356d455fc4c788e17
Misha Dorichenko <misha.dorichenko@gmail.com>
2024-04-14 19:31:42 +0300

    add animation
    Add animation in menu and modal window when it`s close and open.

--- src/js/header.js
+++ src/js/header.js
@@ -5,9 +5,44 @@ titleNavigation.addEventListener('click', titleNavigationHendler);
 
 function titleNavigationHendler(event) {
   event.preventDefault();
-  if (listNavigation.classList.contains('hidden')) {
-    listNavigation.classList.remove('hidden');
-  } else {
-    listNavigation.classList.add('hidden');
-  }
+
+  listNavigation.classList.toggle('show-menu');
 }
+
+/*Section hendler mobile menu*/
+const mobileMenuItems = document.querySelectorAll(
+  '.link-item-navigation-mobile-menu'
+);
+const mobileMenuWrapper = document.querySelector('.mobile-menu-wrapper');
+const burgerMenuOpenBtn = document.querySelector('.mobile-menu-open-btn');
+console.log('burgerMenuOpenBtn' + burgerMenuOpenBtn);
+const burgerMenuCloseBtn = document.querySelector('.mobile-menu-close-btn');
+console.log('burgerMenuCloseBtn' + burgerMenuCloseBtn);
+const orderProjectLinkMobile = document.querySelector(
+  '.link-order_project-navigation-mobile-menu'
+);
+const workTogetherSection = document.querySelector(
+  '.link-order_project-navigation-mobile-menu'
+);
+console.log('workTogetherSection - ' + workTogetherSection);
+
+burgerMenuOpenBtn.addEventListener('click', openMobileMenu);
+burgerMenuCloseBtn.addEventListener('click', closeMobileMenu);
+
+function openMobileMenu() {
+  mobileMenuWrapper.classList.add('modal-open');
+}
+
+function closeMobileMenu() {
+  mobileMenuWrapper.classList.remove('modal-open');
+}
+
+mobileMenuItems.forEach(function (item) {
+  item.addEventListener('click', function () {
+    closeMobileMenu();
+  });
+});
+
+orderProjectLinkMobile.addEventListener('click', function () {
+  closeMobileMenu(); // Закриваємо мобільне меню
+});
