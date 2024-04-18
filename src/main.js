95a54c9a6392599e2317f7d96db149d0583b1e6f
AnnaSamtash <sansara6900@gmail.com>
2024-04-18 13:40:39 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './js/hero.js';
 import './js/about-me.js';
 import './js/benefits.js';
 import './js/projects.js';
-// import './js/faq.js';
+import './js/faq.js';
 import './js/covers.js';
 import './js/reviews.js';
 import './js/work-together.js';
